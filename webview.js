e761500397540fb4022da3f39e290b24726d756f
Mayel de Borniol <git@mayel.space>
2018-08-19 12:22:44 +0300

    direct notif only

--- webview.js
+++ webview.js
@@ -1,13 +1,12 @@
 module.exports = (Franz) => {
   function getMessages() {
     let direct = 0;
-    let indirect = 0;
     const FranzData = document.querySelector('#notifications .badge').innerHTML;
     if (FranzData) {
       direct = FranzData;
     }
 
-    Franz.setBadge(direct, indirect);
+    Franz.setBadge(direct);
   }
 
   Franz.loop(getMessages);
